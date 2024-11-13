32b04925f78ad5189364c34669c598ce05a2a87c
VinayNoogler000 <vinaytambey000@gmail.com>
2024-11-14 00:04:46 +0530

    Feat - Add Animation on Displaying & Hiding the QR-Code, with an Additional 'Generating QR-Code' Feedback using Toast

--- src/app.js
+++ src/app.js
@@ -1,39 +1,60 @@
 // Select HTML DOM Elements:
 const qrFormEl = document.querySelector("#qrForm");
+const txtInpEl = qrFormEl.querySelector("#qrTextInp");
 const qrCodeContainerEl = document.querySelector(".qr-code-container");
 const qrCodeImgEl = qrCodeContainerEl.querySelector("#qrImg");
 
 // Class to Craete Toast Object:
     class Toast {
-        constructor(message, bgColor) {
+        constructor(message, bgColor, duration) {
             this.text = message;
-            this.duration = 3000;
+            this.duration = duration != null ? duration : 3000;
             this.close = true;
             this.gravity =  "top",
             this.position = "center",
             this.stopOnFocus = true,
             this.style = {
                 textAlign: "center",
-                background: bgColor === "green" ? "linear-gradient(to right, #00b09b, #96c93d)" : "linear-gradient(to right, #e33217, #ff001e)",
+                background: bgColor === "green" ? "linear-gradient(to right, #00b09b, #96c93d)" : bgColor === "red" ? "linear-gradient(to right, #e33217, #ff001e)" : "linear-gradient(to right, #ADD8E6, #008080)",
                 cursor: "default",
             }
         }
     }
 
-// Function to Get the QR-Code from an API:
-const displayQrCode = async (qrText) => {
-    const API_URL = "https://api.qrserver.com/v1/create-qr-code/?data="                                        
+// Function to Hide the "QR-Code-Container" when the "Text Input El" is empty:
+const hideQrContainer = (event) => {
+    if(!event.target.value ) {
+        qrCodeContainerEl.classList.remove("show");
+    }
+}
+
+// Function to Show to QR-Container, as the QR-Code-Image gets loaded:
+const displayQrContainer = () => {
+    setTimeout( () => {
+        qrCodeContainerEl.classList.add("show");
+        displayToast("QR-Code Generated Sucessfully!", "green");
+    }, 500);
+}
+
+// Function to Display Toast using Toastify-js:
+const displayToast = (message, bgColor, duration) => {
+    Toastify(new Toast(message, bgColor, duration)).showToast();
+}
+
+// Function to Get the QR-Code:
+const getQrCode = (qrText) => {
+    const API_URL = "https://api.qrserver.com/v1/create-qr-code/?data="                                     
 
     try {
         //Remove starting & trailing spaces of the "qrText", and encode/convert the special characters,
         //making them safe to use in an URL, using "encodeURIComponent()":
         encodedQrText = encodeURIComponent(qrText.trim());
-        qrCodeContainerEl.classList.remove("hidden"); //to display the "QR Container"
         qrCodeImgEl.src = API_URL + encodedQrText;
-        Toastify(new Toast("QR-Code Generated Sucessfully!", "green")).showToast();
+        displayToast("Generating QR Code!", "blue", 1000);   
+        qrCodeImgEl.addEventListener("load", displayQrContainer);
     }
     catch(err) {
-        Toastify(new Toast(`Unable to Get the QR-Code for "${qrText}".\nPlease, try again!`, "red")).showToast();
+        displayToast(`Unable to Get the QR-Code for "${qrText}".\nPlease, try again!`, "red");
         console.log(err);
     }
 }
@@ -48,9 +69,13 @@ const getQrText = () => {
 const submitHandler = (event) => {
     event.preventDefault(); //to prevent the default submission behaviour of form
 
+    qrCodeContainerEl.classList.remove("show"); //hide or reset the "QR-Container"
     const qrText = getQrText();
-    displayQrCode(qrText);
+    getQrCode(qrText);
 }
 
-// Add "Submit" Event Handler for "QrFormEl":
+// Add "Submit" Event Handler for "qrFormEl":
 qrFormEl.addEventListener("submit", submitHandler);
+
+// Add "Change" Event Handler for "inputEl":
+txtInpEl.addEventListener("input", hideQrContainer);
